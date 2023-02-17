f6519d506fc7e2101dced94ae4ade7a9752fff73
Tanvir-Jaber <86012209+Tanvir-Jaber@users.noreply.github.com>
2023-02-17 14:42:46 +0600

    initial

---
+++ app/Http/Controllers/EmployeeContactsController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Employee;
+
+class EmployeeContactsController extends Controller
+{
+    function showContactInfo(Request $req){
+        $employee =  Employee::getInstance()->where('status',1)->where('role','employee')->with(['empDetails','empContacts'])->get();
+        // return $employee; 
+        return view("admin.emergencyContact",compact("employee"));
+    }
+}
