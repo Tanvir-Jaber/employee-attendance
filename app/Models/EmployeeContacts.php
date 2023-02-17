ac0e719936c7bb161f83fad4536144acfcd8b553
Tanvir-Jaber <86012209+Tanvir-Jaber@users.noreply.github.com>
2023-02-17 14:37:52 +0600

    initial

--- app/Models/EmployeeContacts.php
+++ app/Models/EmployeeContacts.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class EmployeeContacts extends Model
+class EmployeeContacts extends SingletonModel
 {
     use HasFactory;
     protected $guarded = [];  
