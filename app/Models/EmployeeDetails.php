d3ee28c2f3c649b604357a75d83726d33f85f83b
Tanvir-Jaber <86012209+Tanvir-Jaber@users.noreply.github.com>
2023-02-17 14:38:13 +0600

    initial

--- app/Models/EmployeeDetails.php
+++ app/Models/EmployeeDetails.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\Employee;
 
-class EmployeeDetails extends Model
+class EmployeeDetails extends SingletonModel
 {
     use HasFactory;
     // protected $fillable = [
